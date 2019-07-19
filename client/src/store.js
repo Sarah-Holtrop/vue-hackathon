7256b554bc6605ec9e230ced6a55d348b831feb2
LynneMartin <lynnemartin@gmail.com>
2019-07-19 15:27:32 -0600

    bootstrap linked

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+let api = axios.create({
+  baseURL: '//localhost:3000/api/posts'
+})
 export default new Vuex.Store({
   state: {
 
--- client/src/views/ThreadPage.vue
+++ client/src/views/ThreadPage.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="Thread page">
+  <div class="post"> <!--NOTE Individual post page-->
+
 
   </div>
 </template>
 
 
 <script>
-  // @ is an alias to /src
-  import HelloWorld from '@/components/HelloWorld.vue'
-
   export default {
-    name: 'home',
-    components: {
-      HelloWorld
-    }
+    name: 'post',
+    data() {
+      return {}
+    },
+    mounted() {
+      let dataToSend = {
+        postId: this.$route.params.postId
+      }
+      // if we need to pass more than one argument, we build a payload object to send "dataToSend"
+      this.$store.dispatch('getPostById', dataToSend)
+    },
+    computed: {
+      post() {
+        return this.$store.state.activePost
+      }
+    },
+    methods: {
+      deletePost() {
+        this.$store.dispatch('deletePost', this.post._id)
+      }
+    },
+    components: {}
   }
 </script>
+
+
+<style scoped>
+
+</style>
